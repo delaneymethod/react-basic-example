e3f7836433c3bb2bb3de98e877953a7107833d09
Sean Delaney <hello@delaneymethod.com>
2019-11-18 09:45:24 +0000

    Fixed imports and tested functional components with React useState hook
    Signed-off-by: Sean Delaney <hello@delaneymethod.com>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
-function App() {
+const App = () => {
+  const [ personsState, setPersonsState ] = useState({
+    persons: [
+      { name: 'Sean', age: 37 },
+      { name: 'Ashley', age: 36 },
+      { name: 'Sionainn', age: 3 },
+      { name: 'Saoirse', age: 1 },
+    ]
+  });
+
+  const switchNameHandler = newName => {
+    setPersonsState({
+      persons: [
+        { name: newName, age: 37 },
+        { name: 'Ashley D', age: 36 },
+        { name: 'Sionainn D', age: 3 },
+        { name: 'Saoirse D', age: 1 },
+      ]
+    });
+  }
+
+  const nameChangedHandler = event => {
+    setPersonsState({
+      persons: [
+        { name: event.target.value, age: 37 },
+        { name: 'Ashley', age: 36 },
+        { name: 'Sionainn', age: 3 },
+        { name: 'Saoirse', age: 1 },
+      ]
+    });
+  }
+
+  // Scoped styling
+  const style = {
+    backgroundColor: 'white',
+    font: 'inherit',
+    border: '1px solid blue',
+    padding: '8px',
+    cursor: 'pointer',
+  };
+
   return (
     <div className="App">
       <h1>React 16 Demo</h1>
+      <button
+        style={style}
+        onClick={switchNameHandler.bind(this, 'Sean!!')}
+      >
+        Switch Name
+      </button>
+      {personsState.persons.map(({ name, age }) => (
+        <Person
+          name={name}
+          age={age}
+          changed={nameChangedHandler}
+        />
+      ))}
     </div>
-  );
+  )
 }
 
 export default App;
